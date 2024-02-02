77e9d08ec99aad2711d2220deba763f892ed7707
kruis <kruis@openLabs>
2024-02-02 10:08:30 +0900

    first update from yk in openlabs

---
+++ demoProject/src/main/java/com/example/demo/DemoProjectApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoProjectApplication {
+	
+	@RequestMapping("/")
+	public String home() {
+		 return "hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectApplication.class, args);
+	}
+
+}
